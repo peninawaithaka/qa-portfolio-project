83e34ebda87e89df2fdeec258ca275f839926752
Penina Waithaka <peninawaithaka5@gmail.com>
2026-05-26 14:59:35 +0300

    added test cases instances

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from models.test_case import *
 
 test_1 = TestCase('Login Functionality', 'Critical', 'In progress')
+test_2 = TestCase('Login UI', 'Medium', 'In progress')
+test_3 = TestCase('Password verification', 'Severe', 'In progress')
 
 print(test_1)
+print(test_3)
